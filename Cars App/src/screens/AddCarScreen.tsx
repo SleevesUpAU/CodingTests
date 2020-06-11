1dbfb513bd5dae8f34b44e06124c902005ba6788
Mateusz Siniarski <mateuszsiniarski@Mateuszs-MacBook-Pro.local>
2020-06-11 21:31:36 +1000

    Add ability to add new cars

--- Cars App/src/screens/AddCarScreen.tsx
+++ Cars App/src/screens/AddCarScreen.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import {Button} from 'react-native';
+import React, {useState} from 'react';
+import {Button, TextInput} from 'react-native';
 import {RootStackScreenProps} from 'src/navigators';
 
 import Screen from 'src/components/Screen';
 
 type Props = RootStackScreenProps<'AddCar'>;
 
-const AddCarScreen = ({navigation}: Props) => (
-  <Screen>
-    <Button
-      title="Add Car"
-      onPress={() => {
-        navigation.goBack();
-      }}
-    />
-  </Screen>
-);
+const AddCarScreen = ({
+  navigation,
+  route: {
+    params: {onAdd},
+  },
+}: Props) => {
+  return (
+    <Screen>
+      <TextInput />
+      <Button
+        title="Add Car"
+        onPress={() => {
+          onAdd({
+            id: 2,
+            initialPrice: 400,
+            currentPrice: 200,
+            company: 'ASDSA',
+          });
+          navigation.goBack();
+        }}
+      />
+    </Screen>
+  );
+};
 
 export default AddCarScreen;
